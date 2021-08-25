024b675590834bca1e649415c104dbcf84870245
[Alex Aloi s3842524] <s3842524@student.rmit.edu.au>
2021-08-25 16:10:08 +1000

    fixed busineness user

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/model/BusinessUser.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/model/BusinessUser.java
@@ -4,7 +4,10 @@ public class BusinessUser extends User {
     private int ABN;
     private String companyName;
 
-    public BusinessUser() {
+    public BusinessUser(Long id, String username, String password, String email, String first_name, String middle_name, String last_name, int rating, int rating_no) {
+        super(id, username, password, email, first_name, middle_name, last_name, rating, rating_no);
+
+
 
     }
 
