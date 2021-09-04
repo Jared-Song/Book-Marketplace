d32c4077e3f63657ed17a2b45a9cfe218cf4d0e5
AiliGong <s3443647@student.rmit.edu.au>
2021-09-04 22:20:50 +1000

    CRD Book

---
+++ FrontEnd/src/components/admin/books/BooksTable.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useSnackbar } from "notistack";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import axios from "axios";
+
+const useStyles = makeStyles((theme) => ({
+  tableContainer: {
+    height: 500,
+  },
+}));
+
+export default function BooksTable({ books, refetch, token }) {
+  const classes = useStyles();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const onDeleteBook = async (bookId) => {
+    try {
+      const {status} = await axios.delete(
+        process.env.NEXT_PUBLIC_BOOK_URL + bookId,
+        { headers: { Authentication: `Bearer ${token}` } }
+      );
+      if (status === 200) {
+        enqueueSnackbar(`Book: ${bookId} has been deleted`, {
+          variant: "success",
+        });
+        refetch();
+      } else {
+        throw "error";
+      }
+    } catch (error) {
+      console.log(error);
+      enqueueSnackbar("Something is wrong!!", {
+        variant: "error",
+      });
+    }
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+    },
+    {
+      field: "action",
+      headerName: " ",
+      disableClickEventBubbling: true,
+      disableColumnMenu: true,
+      disableSelectionOnClick: true,
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <IconButton
+            size="small"
+            onClick={() => {
+              onDeleteBook(params.row.id);
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
+
+  const rows = React.useMemo(() => {
+    return books.map((book) => {
+      return {
+        id: book.id,
+        title: book.title,
+      };
+    });
+  }, [books]);
+
+  return (
+    <div className={classes.tableContainer}>
+      <DataGrid
+        rows={rows}
+        pageSize={10}
+        columns={columns}
+        disableSelectionOnClick
+      />
+    </div>
+  );
+}
--- FrontEnd/src/components/admin/books/CreateBook.js
+++ FrontEnd/src/components/admin/books/CreateBook.js
@@ -2,6 +2,7 @@ import React from "react";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import { makeStyles } from "@material-ui/core/styles";
+import { useSnackbar } from "notistack";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import ListItemText from "@material-ui/core/ListItemText";
@@ -14,6 +15,9 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
 import Slide from "@material-ui/core/Slide";
+import { Controller, useForm } from "react-hook-form";
+import { Box, Grid, TextField } from "@material-ui/core";
+import axios from "axios";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -27,64 +31,121 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(2),
     flex: 1,
   },
+  formContainer: {
+    padding: theme.spacing(2),
+  },
+  dialog: {
+    overflow: "hidden",
+  },
 }));
 
-export default function CreateBook() {
+export default function CreateBook({ token, refetch }) {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      title: "",
+      authorFirstName: "",
+      authorLastName: "",
+      isbn: "",
+      quantity: 0,
+      imageUrl: "",
+      price: 0,
+    },
+  });
+
+  const onCreateBook = async (data) => {
+    try {
+      const { status } = await axios.post(
+        process.env.NEXT_PUBLIC_BOOK_URL + "new",
+        data,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (status === 201) {
+        enqueueSnackbar("Book created!", {
+          variant: "success",
+        });
+        refetch();
+        setOpen(false);
+      } else {
+        throw "Unable to create book";
+      }
+    } catch (error) {
+      enqueueSnackbar("Something is wrong!!", {
+        variant: "error",
+      });
+    }
+  };
 
   return (
     <>
-      <Fab size="medium" color="primary" aria-label="add" onClick={() => {
-        setOpen(true);
-      }}>
+      <Fab
+        size="medium"
+        color="primary"
+        aria-label="add"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
         <AddIcon />
       </Fab>
       <Dialog
         open={open}
+        maxWidth="sm"
+        fullWidth
+        classes={{ paper: classes.dialog }}
         onClose={() => {
           setOpen(false);
         }}
         TransitionComponent={Transition}
       >
-        <AppBar className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              edge="start"
-              color="inherit"
-              onClick={() => {
-                setOpen(false);
-              }}
-              aria-label="close"
-            >
-              <CloseIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              Create Book
-            </Typography>
-            <Button
-              autoFocus
-              color="inherit"
-              onClick={() => {
-                setOpen(false);
-              }}
-            >
-              Save
-            </Button>
-          </Toolbar>
-        </AppBar>
-        <List>
-          <ListItem button>
-            <ListItemText primary="Phone ringtone" secondary="Titania" />
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemText
-              primary="Default notification ringtone"
-              secondary="Tethys"
-            />
-          </ListItem>
-        </List>
+        <form onSubmit={handleSubmit(onCreateBook)}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={() => {
+                  setOpen(false);
+                }}
+                aria-label="close"
+              >
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Create Book
+              </Typography>
+              <Button autoFocus color="inherit" type="submit">
+                Save
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Grid container spacing={2} className={classes.formContainer}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle1">Title</Typography>
+              <Controller
+                name="title"
+                control={control}
+                render={({ field }) => {
+                  return (
+                    <TextField
+                      {...field}
+                      variant="outlined"
+                      fullWidth
+                      margin="dense"
+                    />
+                  );
+                }}
+              />
+            </Grid>
+          </Grid>
+        </form>
       </Dialog>
     </>
   );
