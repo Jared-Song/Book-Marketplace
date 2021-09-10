69e56a6dae08fd4ca6500ed8bd28ac003745a3fd
[Alex Aloi s3842524] <s3842524@student.rmit.edu.au>
2021-09-10 15:46:50 +1000

    fixed csv issues

--- FrontEnd/src/components/csv/CsvDownloadButton.js
+++ FrontEnd/src/components/csv/CsvDownloadButton.js
@@ -10,12 +10,12 @@ export default function CsvDownloadButton({ table }) {
             <Button
                 className={styles.teamName}
                 onClick={() => {
-                var link = document.createElement("a");
-                link.setAttribute("href", encodeURI("data:text/csv;charset=utf-8," + table.map(e => e.join(",")).join("\n")));
-                link.setAttribute("download", "my_data.csv");
-                document.body.appendChild(link); // Required for FF
+                    var link = document.createElement("a");
+                    link.setAttribute("href", encodeURI("data:text/csv;charset=utf-8," + table.map(e => e.join(",")).join("\n")));
+                    link.setAttribute("download", "my_data.csv");
+                    document.body.appendChild(link); // Required for FF
 
-                link.click();
+                    link.click();
                 }}
             >
                 CLICK HERE TO DOWNLOAD CSV
