47dbc6e4fb847e2dc02243dad91b03cf8eaa9ea6
AiliGong <s3443647@student.rmit.edu.au>
2021-09-18 21:26:50 +1000

    Update menu logic

--- FrontEnd/__tests__/homepage.test.js
+++ FrontEnd/__tests__/homepage.test.js
@@ -2,6 +2,19 @@ import "@testing-library/jest-dom/extend-expect";
 import renderer from "react-test-renderer";
 import HomePage from "../src/components/homepage/HomePage";
 
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+    };
+  },
+}));
+
+
 it("renders correctly", () => {
   const tree = renderer.create(<HomePage />).toJSON();
   expect(tree).toMatchSnapshot();
