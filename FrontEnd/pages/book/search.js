93a0ea9357bbfb60a064092a1a995db4d7156a93
AiliGong <s3443647@student.rmit.edu.au>
2021-09-29 00:10:21 +1000

    book search filter

--- FrontEnd/pages/book/search.js
+++ FrontEnd/pages/book/search.js
@@ -13,7 +13,8 @@ import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import MenuItem from "@material-ui/core/MenuItem";
-import _ from "lodash"
+import _ from "lodash";
+import SimpleLoadingPlaceholder from "../../src/components/layouts/SimpleLoadingPlaceholder";
 
 const useStyles = makeStyles((theme) => ({
   list: {
@@ -22,20 +23,44 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-around",
     alignItems: "left",
   },
+  filterAndSortContainer: {
+    marginTop: 40,
+    paddingLeft: 40,
+    display: "flex",
+  },
 }));
 
 export default function Search() {
   const { query } = useRouter();
   const { keyword, filter } = query;
-  const [{ data, loading, error }, refetch] = useAxios(
-    process.env.NEXT_PUBLIC_BROWSE_URL + filter + "/" + keyword
-  );
+
+  const [url, setUrl] = React.useState(null);
+  const [{ data, loading, error }, refetch] = useAxios(url);
   const classes = useStyles();
-  const [order, setOrder] = React.useState('alphabetical');
+  const [order, setOrder] = React.useState("alphabetical");
+  const [conditionFilter, setConditionFilter] = React.useState("all");
+
+  React.useEffect(() => {
+    if (keyword && filter) {
+      if (conditionFilter === "all") {
+        setUrl(process.env.NEXT_PUBLIC_BROWSE_URL + filter + "/" + keyword);
+      } else {
+        setUrl(
+          process.env.NEXT_PUBLIC_BROWSE_URL +
+            "filter/" +
+            conditionFilter +
+            "/" +
+            filter +
+            "/" +
+            keyword
+        );
+      }
+    }
+  }, [keyword, filter, conditionFilter]);
 
   const renderContent = () => {
     if (data && data.length > 0) {
-      let books
+      let books;
       switch (order) {
         case "priceDesc":
           books = _.orderBy(data, ["price"], ["desc"]);
@@ -58,6 +83,19 @@ export default function Search() {
     return <Typography variant="h2">No result found!</Typography>;
   };
 
+  if (loading || !url) {
+    return (
+      <Grid container>
+        <Grid item xs={2}>
+          <BigMenu />
+        </Grid>
+        <Grid item xs={10}>
+          <SimpleLoadingPlaceholder />
+        </Grid>
+      </Grid>
+    );
+  }
+
   return (
     <Grid container>
       <Grid item xs={2}>
@@ -65,8 +103,18 @@ export default function Search() {
       </Grid>
       <Grid item xs={10}>
         <Grid container spacing={2}>
-          <Grid item xs={4} style={{ marginTop: 40, paddingLeft: 40, display: "flex" }}>
-            <Typography style={{whiteSpace: "nowrap", position: "relative", top: 8, paddingRight: 8}}> Sort Order </Typography>
+          <Grid item xs={4} className={classes.filterAndSortContainer}>
+            <Typography
+              style={{
+                whiteSpace: "nowrap",
+                position: "relative",
+                top: 8,
+                paddingRight: 8,
+              }}
+            >
+              {" "}
+              Sort Order{" "}
+            </Typography>
             <FormControl variant="outlined" size="small" fullWidth>
               <Select
                 labelId="order-select-label"
@@ -79,6 +127,33 @@ export default function Search() {
                 <MenuItem value="priceDesc">Price High to Low</MenuItem>
                 <MenuItem value="priceAsc">Price Low to High</MenuItem>
                 <MenuItem value="alphabetical">Alphabetical</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={4} className={classes.filterAndSortContainer}>
+            <Typography
+              style={{
+                whiteSpace: "nowrap",
+                position: "relative",
+                top: 8,
+                paddingRight: 8,
+              }}
+            >
+              {" "}
+              Filter{" "}
+            </Typography>
+            <FormControl variant="outlined" size="small" fullWidth>
+              <Select
+                labelId="order-select-label"
+                defaultValue="all"
+                value={conditionFilter}
+                onChange={(event) => {
+                  setConditionFilter(event.target.value);
+                }}
+              >
+                <MenuItem value="all">All</MenuItem>
+                <MenuItem value="new">New</MenuItem>
+                <MenuItem value="used">Used</MenuItem>
               </Select>
             </FormControl>
           </Grid>
