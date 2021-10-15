f9bba1fdef777185331cd386bb081fa81d1d80b5
Jared Song s3857657 <s3857657@student.rmit.edu.au>
2021-10-16 05:52:57 +1100

    Added logging for books microservice

--- BackEnd/Books/src/main/java/com/rmit/sept/books/web/BookController.java
+++ BackEnd/Books/src/main/java/com/rmit/sept/books/web/BookController.java
@@ -2,12 +2,15 @@ package com.rmit.sept.books.web;
 
 import javax.validation.Valid;
 
+import com.rmit.sept.books.BookApplication;
 import com.rmit.sept.books.model.Book;
 import com.rmit.sept.books.model.User;
 import com.rmit.sept.books.services.BookService;
 import com.rmit.sept.books.services.MapValidationErrorService;
 import com.rmit.sept.books.services.UserService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +23,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/books")
 public class BookController {
+    private static final Logger LOGGER = LogManager.getLogger(BookApplication.class);
 
     @Autowired
     private BookService bookService;
@@ -37,17 +42,21 @@ public class BookController {
     // get all the books in the catalogue
     @GetMapping(path = "/all")
     public Iterable<Book> getAllBooks() {
+        LOGGER.info("Finding all books");
         return bookService.findAllBooks();
     }
 
     // get a book from the catalogue with a specific id
     @GetMapping(path = "/{bookId}")
     public ResponseEntity<?> getBookById(@PathVariable Long bookId) {
+        LOGGER.info("Finding book with ID " + bookId);
         Book book = bookService.findById(bookId);
 
         if (book != null) {
+            LOGGER.info("Found book with ID  " + bookId);
             return new ResponseEntity<Book>(book, HttpStatus.OK);
         } else {
+            LOGGER.warn("Book with ID " + bookId + " was not found");
             return new ResponseEntity<String>("Book with ID " + bookId + " was not found", HttpStatus.NOT_FOUND);
         }
     }
@@ -55,11 +64,14 @@ public class BookController {
     // delete a book in the catalogue with a specific id
     @DeleteMapping(path = "/{bookId}")
     public ResponseEntity<?> deleteBook(@PathVariable Long bookId) {
+        LOGGER.info("Finding book with ID " + bookId);
         Book book = bookService.findById(bookId);
         if (book != null) {
             bookService.deleteBookById(bookId);
+            LOGGER.info("Deleted book with ID " + bookId);
             return new ResponseEntity<String>("Book with ID " + bookId + " was deleted", HttpStatus.OK);
         } else {
+            LOGGER.warn("Book with ID " + bookId + " was not found");
             return new ResponseEntity<String>("Book with ID " + bookId + " was not found", HttpStatus.NOT_FOUND);
         }
 
@@ -68,25 +80,44 @@ public class BookController {
     // add a new book to the catalogue
     @PostMapping("/new")
     public ResponseEntity<?> addNewBook(@Valid @RequestBody Book book, BindingResult result) {
+        LOGGER.info("Adding a new book");
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if (errorMap != null)
+        if (errorMap != null) {
+            LOGGER.warn("The book's information is invalid and the new book cannot be added");
             return errorMap;
+        }
 
-        if (book.getSellerId() == null) return new ResponseEntity<String>("Unable to add the new book, User id not given!.", HttpStatus.NOT_ACCEPTABLE); 
+        if (book.getSellerId() == null) {
+            LOGGER.warn(
+                    "The book's information is invalid and the new book cannot be added - the seller id is not given");
+            return new ResponseEntity<String>("Unable to add the new book, User id not given!.",
+                    HttpStatus.NOT_ACCEPTABLE);
+        }
+
         User user = userService.findById(book.getSellerId());
-        if (user == null) return new ResponseEntity<String>("Unable to add the new book, User to tie to not found!.", HttpStatus.NOT_FOUND);
+        if (user == null) {
+            LOGGER.warn(
+                    "The book's information is invalid and the new book cannot be added - the seller does not exist");
+            return new ResponseEntity<String>("Unable to add the new book, User to tie to not found!.",
+                    HttpStatus.NOT_FOUND);
+        }
+
         book.setSeller(user);
         Book newBook = bookService.saveBook(book);
         if (newBook != null) {
+            LOGGER.info("The new book has been successfully added");
             return new ResponseEntity<Book>(newBook, HttpStatus.OK);
         } else {
-            return new ResponseEntity<String>("Unable to add the new book, a copy of the book already exists.", HttpStatus.CONFLICT);
+            LOGGER.warn("The new book could not be added as a copy of it already exists");
+            return new ResponseEntity<String>("Unable to add the new book, a copy of the book already exists.",
+                    HttpStatus.CONFLICT);
         }
     }
 
     // get all the books in the catalogue from a seller
     @GetMapping(path = "/sellerId/{sellerId}")
     public Iterable<Book> getAllBooksBySeller(@PathVariable Long sellerId) {
+        LOGGER.info("Finding all books that are sold by the seller with ID " + sellerId);
         return bookService.getAllBySeller(userService.findById(sellerId));
     }
 }
--- BackEnd/Books/src/main/java/com/rmit/sept/books/web/EditBookController.java
+++ BackEnd/Books/src/main/java/com/rmit/sept/books/web/EditBookController.java
@@ -1,10 +1,13 @@
 package com.rmit.sept.books.web;
 
+import com.rmit.sept.books.BookApplication;
 import com.rmit.sept.books.model.Book;
 import com.rmit.sept.books.model.BookForm;
 import com.rmit.sept.books.services.BookService;
 import com.rmit.sept.books.services.EditBookService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/editBook")
 public class EditBookController {
+    private static final Logger LOGGER = LogManager.getLogger(BookApplication.class);
 
     @Autowired
     private BookService bookService;
@@ -28,20 +33,28 @@ public class EditBookController {
     // edit a book's details
     @PostMapping("/{Id}")
     public ResponseEntity<?> editBook(@RequestBody BookForm bookForm, @PathVariable Long Id) {
+        LOGGER.info("Finding book with ID " + Id);
         Book book = bookService.findById(Id);
         if (book != null) {
             if (bookForm.getSeller() == null && bookForm.getSellerId() == null) {
-                return new ResponseEntity<String>("Unable to add the new book, User to tie to not found!.", HttpStatus.NOT_FOUND);
+                LOGGER.warn(
+                        "The book's information is invalid and the new book cannot be added - the seller does not exist");
+                return new ResponseEntity<String>("Unable to add the new book details, User to tie to not found!.",
+                        HttpStatus.NOT_FOUND);
             }
             Book updateBook = editBookService.updateBook(bookForm, book);
             if (updateBook != null) {
+                LOGGER.info("The details have been successfully updated for the book with ID " + Id);
                 return new ResponseEntity<String>("Successfully updated book details", HttpStatus.OK);
             } else {
+                LOGGER.warn(
+                        "The edited book details could not be saved as another book with the same details already exists");
                 return new ResponseEntity<String>("Unable to save details for book, a copy of the book already exists.",
                         HttpStatus.CONFLICT);
             }
 
         } else {
+            LOGGER.warn("Book with ID " + Id + " was not found");
             return new ResponseEntity<String>("Book with ID " + Id + " was not found", HttpStatus.NOT_FOUND);
         }
     }
