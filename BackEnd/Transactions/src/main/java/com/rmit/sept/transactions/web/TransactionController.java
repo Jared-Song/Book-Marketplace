2efbc6baba382b63dcccf76f292fb05028238293
Jared Song s3857657 <s3857657@student.rmit.edu.au>
2021-10-16 23:00:32 +1100

    Added logging to transactions microservice

--- BackEnd/Transactions/src/main/java/com/rmit/sept/transactions/web/TransactionController.java
+++ BackEnd/Transactions/src/main/java/com/rmit/sept/transactions/web/TransactionController.java
@@ -2,12 +2,15 @@ package com.rmit.sept.transactions.web;
 
 import javax.validation.Valid;
 
+import com.rmit.sept.transactions.TransactionsApplication;
 import com.rmit.sept.transactions.exceptions.NotFoundException;
 import com.rmit.sept.transactions.model.Transaction;
 import com.rmit.sept.transactions.model.TransactionStatus;
 import com.rmit.sept.transactions.services.MapValidationErrorService;
 import com.rmit.sept.transactions.services.TransactionService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,10 +24,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/transactions")
 public class TransactionController {
+    private static final Logger LOGGER = LogManager.getLogger(TransactionsApplication.class);
+
     @Autowired
     private TransactionService transactionService;
 
@@ -34,10 +40,12 @@ public class TransactionController {
     // get all transactions within the system
     @GetMapping(path = "/all")
     public ResponseEntity<?> getAllTransactions() {
+        LOGGER.trace("Finding all transactions");
         Iterable<Transaction> transactions = transactionService.findAllTransactions();
-        System.out.println(transactions);
-        //test if any transactions were found
-        if(!transactions.iterator().hasNext()){
+
+        // test if any transactions were found
+        if (!transactions.iterator().hasNext()) {
+            LOGGER.warn("No transactions were found");
             throw new NotFoundException("No transactions found");
         }
         return new ResponseEntity<Iterable<Transaction>>(transactions, HttpStatus.OK);
@@ -45,44 +53,53 @@ public class TransactionController {
 
     // get a transaction by its transaction id
     @GetMapping(path = "/{Id}")
-    public ResponseEntity<?> getTransactionById(@PathVariable Long Id) {
-        Transaction transaction = transactionService.findById(Id);
-        //test if a transaction was found
-        if(transaction == null){
-            throw new NotFoundException("Transaction with ID '" + Id + "' does not exist");
+    public ResponseEntity<?> getTransactionById(@PathVariable Long transactionId) {
+        LOGGER.trace("Finding transaction with ID " + transactionId);
+        Transaction transaction = transactionService.findById(transactionId);
+        // test if a transaction was found
+        if (transaction == null) {
+            LOGGER.warn("Transaction with ID " + transactionId + " was not found");
+            throw new NotFoundException("Transaction with ID '" + transactionId + "' does not exist");
         }
         return new ResponseEntity<Transaction>(transaction, HttpStatus.OK);
     }
 
     // delete a transaction by its transaction id
     @DeleteMapping(path = "/{Id}")
-    public ResponseEntity<?> deleteTransaction(@PathVariable Long Id) {
-        Transaction transaction = transactionService.findById(Id);
-        //test if a transaction was found
-        if(transaction == null){
-            throw new NotFoundException("Transaction with ID '" + Id + "' does not exist");
+    public ResponseEntity<?> deleteTransaction(@PathVariable Long transactionId) {
+        LOGGER.trace("Finding transaction with ID " + transactionId);
+        Transaction transaction = transactionService.findById(transactionId);
+        // test if a transaction was found
+        if (transaction == null) {
+            LOGGER.warn("Transaction with ID " + transactionId + " was not found");
+            throw new NotFoundException("Transaction with ID '" + transactionId + "' does not exist");
         }
-        transactionService.deleteTransactionById(Id);
-        return new ResponseEntity<String>("Transaction with ID " + Id + " was deleted", HttpStatus.OK);
+        LOGGER.trace("Deleting transaction with ID " + transactionId);
+        transactionService.deleteTransactionById(transactionId);
+        return new ResponseEntity<String>("Transaction with ID " + transactionId + " was deleted", HttpStatus.OK);
     }
 
     // get transactions by buyer ID
     @GetMapping(path = "/buyer/{buyerID}")
-    public ResponseEntity<?> getAllTransactionByBuyerID(@PathVariable Long buyerID) {
-        Iterable<Transaction> transactions = transactionService.getAllByBuyerID(buyerID);
-        if(!transactions.iterator().hasNext()){
-            throw new NotFoundException("No transactions found with buyer ID '" + buyerID + "'");
+    public ResponseEntity<?> getAllTransactionByBuyerId(@PathVariable Long buyerId) {
+        LOGGER.trace("Finding transaction with the buyer ID " + buyerId);
+        Iterable<Transaction> transactions = transactionService.getAllByBuyerID(buyerId);
+        if (!transactions.iterator().hasNext()) {
+            LOGGER.warn("No transactions were found with buyer ID " + buyerId);
+            throw new NotFoundException("No transactions found with buyer ID '" + buyerId + "'");
         }
         return new ResponseEntity<Iterable<Transaction>>(transactions, HttpStatus.OK);
     }
 
-    //get transactions by seller ID
+    // get transactions by seller ID
     @GetMapping(path = "/seller/{sellerID}")
-    public ResponseEntity<?> getAllTransactionBySellerID(@PathVariable Long sellerID) {
-        Iterable<Transaction> transactions = transactionService.getAllBySellerID(sellerID);
-        //test if any transactions were found
-        if(!transactions.iterator().hasNext()){
-            throw new NotFoundException("No transactions found with seller ID '" + sellerID + "'");
+    public ResponseEntity<?> getAllTransactionBySellerId(@PathVariable Long sellerId) {
+        LOGGER.trace("Finding transaction with the buyer ID " + sellerId);
+        Iterable<Transaction> transactions = transactionService.getAllBySellerID(sellerId);
+        // test if any transactions were found
+        if (!transactions.iterator().hasNext()) {
+            LOGGER.warn("No transactions were found with seller ID " + sellerId);
+            throw new NotFoundException("No transactions found with seller ID '" + sellerId + "'");
         }
         return new ResponseEntity<Iterable<Transaction>>(transactions, HttpStatus.OK);
     }
@@ -91,43 +108,54 @@ public class TransactionController {
     @PostMapping("/new")
     public ResponseEntity<?> createNewTransaction(@Valid @RequestBody Transaction transaction, BindingResult result) {
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
-        if (errorMap != null)
+        if (errorMap != null) {
+            LOGGER.warn("The transaction's details are invalid and the transaction cannot be added");
             return errorMap;
-
+        }
+        LOGGER.trace("The new transaction has been successfully added");
         Transaction newTransaction = transactionService.saveTransaction(transaction);
-
         return new ResponseEntity<Transaction>(newTransaction, HttpStatus.CREATED);
     }
 
     // update the status of a transaction
     @PostMapping("/updateStatus/{transactionId}")
     @ResponseBody
-    public ResponseEntity<?> updateTransactionStatus(@RequestBody Transaction transaction, @PathVariable Long transactionId) {
+    public ResponseEntity<?> updateTransactionStatus(@RequestBody Transaction transaction,
+            @PathVariable Long transactionId) {
+
         TransactionStatus status = transaction.getStatus();
+        LOGGER.trace("Finding transaction with ID " + transactionId);
         Transaction transaction2 = transactionService.findById(transactionId);
         if (transaction2 != null) {
+            LOGGER.trace("Updating the status for the transaction with ID " + transactionId);
             Transaction updateTransaction = transactionService.updateTransactionStatus(status, transaction2);
             return new ResponseEntity<Transaction>(updateTransaction, HttpStatus.OK);
         } else {
-           throw new NotFoundException("Transaction with ID " + transactionId + " was not found");
+            LOGGER.warn("transaction with ID " + transactionId + " was not found");
+            throw new NotFoundException("Transaction with ID " + transactionId + " was not found");
         }
     }
 
     // refund transaction by id
     @GetMapping("/refund/{transactionId}")
     public ResponseEntity<?> refundTransaction(@PathVariable Long transactionId) {
+        LOGGER.trace("Finding transaction with ID " + transactionId);
         Transaction transaction = transactionService.findById(transactionId);
         // test if a transaction was found
-        if(transaction == null){
+        if (transaction == null) {
+            LOGGER.warn("transaction with ID " + transactionId + " was not found");
             throw new NotFoundException("Transaction with ID '" + transactionId + "' does not exist");
         }
+        LOGGER.trace("Refunding transaction with ID " + transactionId);
         boolean refunded = transactionService.refundTransaction(transaction);
         if (refunded) {
+            LOGGER.trace("Refund was successful for transaction with ID " + transactionId);
             return new ResponseEntity<String>("Refund successful.", HttpStatus.OK);
         } else {
+            LOGGER.trace("Refund request was created for transaction with ID " + transactionId);
             return new ResponseEntity<String>("Refund request was created.", HttpStatus.OK);
         }
-        
+
     }
 
 }
