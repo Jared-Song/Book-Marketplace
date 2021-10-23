a821161d99f0bdf7cdd1c79d0b94284680aa4ed1
Jared Song s3857657 <s3857657@student.rmit.edu.au>
2021-10-23 21:14:24 +1100

    Added logs to reviews service

--- BackEnd/Reviews/src/main/java/com/rmit/sept/reviews/web/ReviewController.java
+++ BackEnd/Reviews/src/main/java/com/rmit/sept/reviews/web/ReviewController.java
@@ -2,12 +2,15 @@ package com.rmit.sept.reviews.web;
 
 import javax.validation.Valid;
 
+import com.rmit.sept.reviews.ReviewsApplication;
 import com.rmit.sept.reviews.model.Review;
 import com.rmit.sept.reviews.model.Transaction;
 import com.rmit.sept.reviews.services.MapValidationErrorService;
 import com.rmit.sept.reviews.services.ReviewService;
 import com.rmit.sept.reviews.services.TransactionService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +28,8 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/review")
 public class ReviewController {
-    
+    private static final Logger LOGGER = LogManager.getLogger(ReviewsApplication.class);
+
     @Autowired
     private TransactionService transactionService;
     // adds the service to review a book
@@ -42,43 +46,63 @@ public class ReviewController {
 
     @PostMapping("/addReview")
     public ResponseEntity<?> addReview(@Valid @RequestBody Review review, BindingResult result) {
+        LOGGER.warn("Adding a new review");
 
-        //check if there's a mapping error
+        // check if there's a mapping error
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
         if (errorMap != null) {
+            LOGGER.warn("Review was invalid and unable to be added");
             return errorMap;
         }
 
-        if (review.getTransactionId() == null)
-            return new ResponseEntity<String>("Unable to add the new user review, transaction id not given!.", HttpStatus.NOT_ACCEPTABLE);
+        // if transaction does not exist, return error
+        if (review.getTransactionId() == null) {
+            LOGGER.warn("Review was missing transaction id and unable to be added");
+            return new ResponseEntity<String>("Unable to add the new user review, transaction id not given!.",
+                    HttpStatus.NOT_ACCEPTABLE);
+        }
 
         Transaction transaction = transactionService.findById(review.getTransactionId());
 
-        if (transaction == null)
-            return new ResponseEntity<String>("Unable to add the user review, transaction ID to not found!.", HttpStatus.NOT_FOUND);
-
+        // if transaction does not exist, return error
+        if (transaction == null) {
+            LOGGER.warn("Review was missing transaction id and unable to be added");
+            return new ResponseEntity<String>("Unable to add the user review, transaction not found!.",
+                    HttpStatus.NOT_FOUND);
+        }
         review.setTransaction(transaction);
 
+        // if transaction has been reviewed already, do not review it
         if (transaction.getIsReviewed()) {
-            return new ResponseEntity<String>("Unable to add the user review, transaction has already been reviewed!.", HttpStatus.NOT_ACCEPTABLE);
+            LOGGER.warn("Review was unable to be added because it was reviewed already");
+            return new ResponseEntity<String>("Unable to add the user review, transaction has already been reviewed!.",
+                    HttpStatus.NOT_ACCEPTABLE);
         }
 
         Review userReview = reviewService.addReview(review);
 
+        // return response success if review was successfully saved
         if (userReview == null) {
-            return new ResponseEntity<String>("Review failed to be added! " + String.valueOf(review.getId()) + " ", HttpStatus.CONFLICT);
+            LOGGER.trace("Review failed to be added");
+            return new ResponseEntity<String>("Review failed to be added! " + String.valueOf(review.getId()) + " ",
+                    HttpStatus.CONFLICT);
         } else {
             reviewService.incrementRating(review);
-            return new ResponseEntity<String>("Review with ID " + String.valueOf(review.getId()) + " was added", HttpStatus.OK);
+            LOGGER.trace("Review was successfully added");
+            return new ResponseEntity<String>("Review with ID " + String.valueOf(review.getId()) + " was added",
+                    HttpStatus.OK);
         }
     }
 
     @DeleteMapping("/deleteReview/{reviewID}")
     public ResponseEntity<?> delete(@PathVariable Long reviewID) {
         if (reviewService.deleteReview(reviewID)) {
+            LOGGER.trace("Review was successfully deleted");
             return new ResponseEntity<String>("Review with ID " + reviewID + " was deleted", HttpStatus.OK);
         } else {
-            return new ResponseEntity<String>("Review with ID " + reviewID + " failed to be deleted", HttpStatus.NOT_ACCEPTABLE);
+            LOGGER.warn("Review was unable to be deleted");
+            return new ResponseEntity<String>("Review with ID " + reviewID + " failed to be deleted",
+                    HttpStatus.NOT_ACCEPTABLE);
         }
 
     }
